13f93734e978bfe2ed74983cc94020b1652b9a25
Brian Wilson <brian@wiltech.org>
2018-04-01 11:21:38 -0400

    Refactoring/fixed typo in PKGBUILD

--- wiltech-repo.sh
+++ wiltech-repo.sh
@@ -14,19 +14,19 @@ REPO_PATH=repo/x86_64
 REPO_NAME=wiltech
 
 mkdir -p "$CHROOT"
-[[ -d "$CHROOT/root" ]] || mkarchroot -C /etc/pacman.conf $CHROOT/root base base-devel
+[[ -d "$CHROOT/root" ]] || mkarchroot -C /etc/pacman.conf $CHROOT/root base base-devel oh-my-zsh-git
 
 for package in $PACKAGES; do
     cd "$package"
     rm -f *.pkg.tar.xz
-    makechrootpkg -cur $CHROOT
+    makechrootpkg -curn $CHROOT
     cd -
 done
 
 repo="$(mktemp -d)"
 defer "rmdir '$repo'"
 
-sshfs -o idmap=user,IdentityFile=/home/doc/.ssh/id_rsa root@${SSHFS_HOST}:/var/www/html "$repo"
+sshfs -o idmap=user,IdentityFile=/home/doc/.ssh/id_rsa doc@${SSHFS_HOST}:/var/www/html "$repo"
 defer "fusermount -u '$repo'"
 
 rsync --ignore-existing -v pkgbuild/*/*.pkg.tar.xz "$repo/$REPO_PATH"
